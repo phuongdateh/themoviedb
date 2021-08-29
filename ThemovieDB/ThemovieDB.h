10bf9eeb0ac9c5824b7be91d33e59002fc53da0a
phuong.doan <95doanphuong@gamil.com>
2021-08-29 16:56:23 +0700

    Init Project

---
+++ ThemovieDB/ThemovieDB.h
@@ -0,0 +1,18 @@
+//
+//  ThemovieDB.h
+//  ThemovieDB
+//
+//  Created by PhuongDoan on 29/08/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ThemovieDB.
+FOUNDATION_EXPORT double ThemovieDBVersionNumber;
+
+//! Project version string for ThemovieDB.
+FOUNDATION_EXPORT const unsigned char ThemovieDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThemovieDB/PublicHeader.h>
+
+
